f09e530e3868d606bca4ac2f8d5309837c8d88cd
2016HT13247 <2015ht12345@wilp.bits-pilani.ac.in>
2017-12-20 02:36:04 +0530

    tv single halfway done

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse, JsonResponse
 import json
-import urllib
+import urllib.request
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.cache import cache
 from django.contrib.auth.models import User
@@ -14,7 +14,7 @@ from django.views.decorators.csrf import csrf_exempt
 api = '4a95b57fbcd4eea6c3e07a72ee861599'
 lang = 'en-US'
 genreURL = 'https://api.themoviedb.org/3/genre/movie/list?api_key='+api+'&language='+lang
-response0 = urllib.urlopen(genreURL)
+response0 = urllib.request.urlopen(genreURL)
 genre = json.loads(response0.read())
 genre = genre['genres']
 
@@ -77,7 +77,7 @@ def index(request):
 	if not popmovie:
 		popmovie= []
 		popurl = 'https://api.themoviedb.org/3/movie/popular?api_key='+api+'&language='+lang+'&page=1'
-		response0 = urllib.urlopen(popurl)
+		response0 = urllib.request.urlopen(popurl)
 		pop = json.loads(response0.read())
 		for i in pop['results']:
 			i['genres']=[]
@@ -95,7 +95,7 @@ def index(request):
 	if not nowmovie:
 		nowmovie=[]
 		nowurl= 'https://api.themoviedb.org/3/movie/now_playing?api_key='+api+'&language='+lang+'&page=1'
-		response0 = urllib.urlopen(nowurl)
+		response0 = urllib.request.urlopen(nowurl)
 		now = json.loads(response0.read())
 		for i in now['results']:
 			i['genres']=[]
@@ -112,7 +112,7 @@ def index(request):
 	if not topmovie:
 		topmovie=[]
 		topurl= 'https://api.themoviedb.org/3/movie/top_rated?api_key='+api+'&language='+lang+'&page=1'
-		response0 = urllib.urlopen(topurl)
+		response0 = urllib.request.urlopen(topurl)
 		top = json.loads(response0.read())
 		for i in top['results']:
 			i['genres']=[]
@@ -129,7 +129,7 @@ def index(request):
 	if not upmovie:
 		upmovie=[]
 		upurl= 'https://api.themoviedb.org/3/movie/upcoming?api_key='+api+'&language='+lang+'&page=1'
-		response0 = urllib.urlopen(upurl)
+		response0 = urllib.request.urlopen(upurl)
 		up = json.loads(response0.read())
 		for i in up['results']:
 			i['genres']=[]
@@ -152,7 +152,7 @@ def index(request):
 	if not airtodayshow:
 		airtodayshow= []
 		airtodayurl = 'https://api.themoviedb.org/3/tv/airing_today?api_key='+api+'&language='+lang+'&page=1'
-		response0 = urllib.urlopen(airtodayurl)
+		response0 = urllib.request.urlopen(airtodayurl)
 		airtoday = json.loads(response0.read())
 		for i in airtoday['results']:
 			i['genres']=[]
@@ -169,7 +169,7 @@ def index(request):
 	if not airshow:
 		airshow=[]
 		airurl= 'https://api.themoviedb.org/3/tv/on_the_air?api_key='+api+'&language='+lang+'&page=1'
-		response0 = urllib.urlopen(airurl)
+		response0 = urllib.request.urlopen(airurl)
 		air = json.loads(response0.read())
 		for i in air['results']:
 			i['genres']=[]
@@ -186,7 +186,7 @@ def index(request):
 	if not popshow:
 		popshow=[]
 		popurl= 'https://api.themoviedb.org/3/tv/popular?api_key='+api+'&language='+lang+'&page=1'
-		response0 = urllib.urlopen(popurl)
+		response0 = urllib.request.urlopen(popurl)
 		pop = json.loads(response0.read())
 		for i in pop['results']:
 			i['genres']=[]
@@ -203,7 +203,7 @@ def index(request):
 	if not topshow:
 		topshow=[]
 		topurl= 'https://api.themoviedb.org/3/tv/top_rated?api_key='+api+'&language='+lang+'&page=1'
-		response0 = urllib.urlopen(topurl)
+		response0 = urllib.request.urlopen(topurl)
 		top = json.loads(response0.read())
 		for i in top['results']:
 			i['genres']=[]
@@ -235,7 +235,7 @@ def movielist(request):
 			popmovie= []
 			for i in range(1,11):			#ignore this part
 				popurl = 'https://api.themoviedb.org/3/movie/popular?api_key='+api+'&language='+lang+'&page='+str(i) #develop the api
-				response0 = urllib.urlopen(popurl) 	#call the api
+				response0 = urllib.request.urlopen(popurl) 	#call the api
 				pop = json.loads(response0.read())	#
 				for i in pop['results']:
 					i['genres']=[]
@@ -253,10 +253,10 @@ def movielist(request):
 		return render(request, 'moviegrid.html', context)
 	else:
 		return redirect('/login/')
-		
+
 def movie(request, movie_id):
 	movurl = 'https://api.themoviedb.org/3/movie/'+movie_id+'?api_key='+api+'&language='+lang+'&page=1'
-	response0 = urllib.request.urlopen(movurl)
+	response0 = urllib.request.request.urlopen(movurl)
 	mov = json.loads(response0.read())
 	date=mov['release_date']
 	date=date[:4]
@@ -271,7 +271,7 @@ def movie(request, movie_id):
 	for c in crew:
 		if(c['job']=='Director'):
 			director=c
-		elif(c['job']=='Story'):
+		elif(c['department']=='Writing'):
 			writers.append(c)
 		elif(c['department']=='Production'):
 			producers.append(c)
@@ -296,7 +296,54 @@ def movie(request, movie_id):
 	return render(request, 'moviesingle.html',context)
 
 def show(request, show_id):
-	return render(request, 'seriessingle.html')
+	showurl = 'https://api.themoviedb.org/3/tv/'+show_id+'?api_key='+api+'&language='+lang
+	response0 = urllib.request.urlopen(showurl)
+	showdet = json.loads(response0.read())
+	date=showdet['first_air_date']
+	date=date[:4]
+	if(showdet['in_production']):
+		enddate='current'
+	else:
+		enddate=showdet['last_air_date']
+		enddate=enddate[:4]
+	seasons=showdet['seasons']
+	curr=seasons[-1]
+
+	showsimurl= 'https://api.themoviedb.org/3/tv/'+show_id+'/similar?api_key='+api+'&language='+lang+'&page=1'
+	response0 = urllib.request.urlopen(showsimurl)
+	showsim = json.loads(response0.read())
+	showsim=showsim['results']
+
+	showcredurl= 'https://api.themoviedb.org/3/tv/'+show_id+'/credits?api_key='+api+'&language='+lang+'&page=1'
+	response0 = urllib.request.urlopen(showcredurl)
+	showcred = json.loads(response0.read())
+	cast=showcred['cast']
+	crew=showcred['crew']
+	writers=[]
+	producers=[]
+	director=""
+	for c in crew:
+		if(c['job']=='Director'):
+			director=c
+		elif(c['department']=='Writing'):
+			writers.append(c)
+		elif(c['department']=='Production'):
+			producers.append(c)
+
+
+
+	context={
+		'detail':showdet,
+		'showsim':showsim,
+		'director':director,
+		'writers':writers,
+		'producers':producers,
+		'cast':cast,
+		'date': date,
+		'enddate': enddate,
+		'curr':curr,
+	}
+	return render(request, 'seriessingle.html', context)
 
 def tvlist(request):
 	if request.user.is_authenticated():		
