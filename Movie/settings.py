b4abe049351d1423b315f7fcfe8df5e2f2b78f46
DoodleCoder <gaurav290497@gmail.com>
2018-01-03 23:44:13 +0530

    Project changes

--- Movie/settings.py
+++ Movie/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'grmovies.urls'
+ROOT_URLCONF = 'Movie.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'grmovies.wsgi.application'
+WSGI_APPLICATION = 'Movie.wsgi.application'
 
 
 # Database
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grmovies.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Movie.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
